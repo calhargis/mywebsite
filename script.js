112282a2a344121d78fc72f6e7a10ca1c17f7947
calhargis <calhargis1@gmail.com>
2022-07-01 05:49:24 -0600

    added new graphics and resume

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ function showMenu() {
 function hideMenu() {
     navLinks.style.right = "-200px";
 }
+
+function darkMode() {
+    var element = document.body;
+    if (element.getAttribute(body.darkMode)) {
+        element.className = "light-mode";
+        return false;
+    }
+    element.className = "dark-mode";
+    return false;
+  }
+
+  function lightMode() {
+    var element = document.body;
+    element.className = "light-mode";
+  }
